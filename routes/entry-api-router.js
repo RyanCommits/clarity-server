84e19661c2055ef87202ba776e3fbd6c5dd1c1c0
Ryan Wang <qwang1993@gmail.com>
2017-10-10 17:12:06 -0400

    put request works

--- routes/entry-api-router.js
+++ routes/entry-api-router.js
@@ -16,7 +16,7 @@ router.get('/dashboard', (req, res, next) => {
     }
 
     EntryModel.find(
-       {} // { user: req.user._id }
+        { user: req.user._id }
     )
         // .limit(7)
         .sort({ date: 1 })
@@ -31,6 +31,31 @@ router.get('/dashboard', (req, res, next) => {
         });
 });
 
+// get one entry to edit
+router.get('/dashboard/edit/:year/:month/:date', (req, res, next) => {
+    if (!req.user) {
+        res.status(401).json({ errorMessage: 'Not logged in.' });
+        return;
+    }
+
+    EntryModel.findOne({
+        $and: [
+            { user: req.user._id },
+            // find by date
+            { date: { $eq: req.params.year + '-' + req.params.month + '-' + req.params.date }}
+        ]
+    })
+        .exec((err, oneEntry) => {
+            if (err) {
+                console.log('Error finding entries', err);
+                res.status(500).json({ errorMessage: 'Finding entries went wrong'});
+                return;
+            }
+            res.status(200).json(oneEntry);
+        });
+});
+
+// make new entry
 router.post('/dashboard/new/:year/:month/:date', (req, res, next) => {
     
     if (!req.user) {
@@ -71,6 +96,48 @@ router.post('/dashboard/new/:year/:month/:date', (req, res, next) => {
 
         res.status(200).json(theEntry);
     });
+});
+
+router.put('/dashboard/edit/:year/:month/:date', (req, res, next) => {
+    
+    if (!req.user) {
+        res.status(401).json({ errorMessage: 'Not logged in.' });
+        return;
+    }
+
+    const theEntry = {
+        date: req.body.entryDate,
+        // image: req.body.image,
+        grateful: req.body.entryGrateful,
+        willAccomplish: req.body.entryWillAccomplish,
+        affirmation: req.body.entryAffirmation,
+        dump: req.body.entryDump ,
+        rating: req.body.entryRating,
+        achievements: req.body.entryAchievements,
+        learn: req.body.entryLearn,
+        improve: req.body.entryImprove
+    };
+    // Upload image
+    // if (req.file) {
+    //     thePhone.image = '/uploads/' + req.file.filename;
+    // }
+    EntryModel.findOneAndUpdate({
+        $and: [
+            { user: req.user._id },
+            // find by date
+            { date: { $eq: req.params.year + '-' + req.params.month + '-' + req.params.date }}
+        ]
+    },
+    theEntry,
+    (err) => {
+        if (err) {
+            console.log('Error updating entry', err);
+            res.status(500).json({ errorMessage: 'New entry went wrong' })
+        }
+
+        res.status(200).json(theEntry);
+    });
+   
 });
 
 module.exports = router;
